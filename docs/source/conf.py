5b47c1b3f5dec09d25f59bdbb9020bb9940a902b
Mayank Jain <mayank10.j@gmail.com>
2019-11-26 17:09:22 +0530

    Updated master_doc to index in sphinx docs build: to remove error Sphinx Error contents.rst not found

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,7 @@ import sys
 import sphinx_rtd_theme
 
 
-# -- Set start point ---------------------
-master_doc = 'index'
+
 
 sys.path.insert(0, os.path.abspath(os.path.join('..','..','ai')))
 
@@ -78,3 +77,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Set start point ---------------------
+master_doc = 'index'
